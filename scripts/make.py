4f50090183b72db2255bebdb6f1f25ab1f650c53
Urban Alič <urbanalich@gmail.com>
2025-12-06 19:40:16 +0100

    IMPROVE: packaging performance

--- scripts/make.py
+++ scripts/make.py
@@ -1,44 +1,51 @@
-import json
 import os
 import shutil
 import subprocess
+import time
 
 from neuro.utils import config, internal_utils
+from neuro.tools.local import assemble
+from neuro.tools.terminal import style
 
 
 def copy_nwjs():
     print("Building: NW.js")
-    nwjs_dir = f"{internal_utils.get_path("desktop")}/nwjs/"
-    nwjs_source_path = f"{nwjs_dir}/v{os.getenv('NWJS_VERSION')}"
-    shutil.copytree(nwjs_source_path, BUILD_DIR)
+    nwjs_dir = internal_utils.get_path("desktop") + "/nwjs"
+    nwjs_source_path = f"{nwjs_dir}/v{os.getenv('NWJS_VERSION')}/"
+    rsync_copy_command = [
+        "rsync",
+        "-a",
+        "--delete",
+        nwjs_source_path,
+        BUILD_DIR
+    ]
+    subprocess.run(rsync_copy_command, check=True, stdout=subprocess.DEVNULL)
 
 
 def copy_tw5():
     print("Building: TW5")
     tw5_source_path = internal_utils.get_path("tw5")
-    shutil.rmtree(f"{BUILD_DIR}/tw5", ignore_errors=True)
-    shutil.copytree(tw5_source_path, f"{BUILD_DIR}/tw5")
-
-
-def copy_plugins_and_themes():
-    print("Building: plugins and themes")
-    for plugin in json.loads(os.getenv("EXTERNAL_PLUGINS")):
-        plugin_source_path = plugin["path"]
-        plugin_target_path = internal_utils.get_path("desktop") + "/build/tw5/plugins/" + plugin["name"]
-        shutil.rmtree(plugin_target_path, ignore_errors=True)
-        shutil.copytree(plugin_source_path, plugin_target_path)
-
-    for theme in json.loads(os.getenv("EXTERNAL_THEMES")):
-        theme_source_path = theme["path"]
-        theme_target_path = internal_utils.get_path("desktop") + "/build/tw5/themes/" + theme["name"]
-        shutil.rmtree(theme_target_path)
-        shutil.copytree(theme_source_path, theme_target_path)
+    rsync_copy_command = [
+        "rsync",
+        "-a",
+        "--delete",
+        tw5_source_path,
+        BUILD_DIR
+    ]
+    subprocess.run(rsync_copy_command, check=True, stdout=subprocess.DEVNULL)
 
 
 def copy_source():
     print("Building: source")
     source = internal_utils.get_path("desktop") + "/source"
-    shutil.copytree(source, f"{BUILD_DIR}/source")
+    rsync_copy_command = [
+        "rsync",
+        "-a",
+        "--delete",
+        source,
+        BUILD_DIR
+    ]
+    subprocess.run(rsync_copy_command, check=True, stdout=subprocess.DEVNULL)
     shutil.move(f"{BUILD_DIR}/source/package.json", f"{BUILD_DIR}/package.json")
 
 
@@ -54,12 +61,15 @@ def install_node_modules():
 
 
 def main():
-    shutil.rmtree(BUILD_DIR, ignore_errors=True)
+    start_time = time.time()
+    os.makedirs(BUILD_DIR, exist_ok=True)
     copy_nwjs()
     copy_tw5()
-    copy_plugins_and_themes()
+    assemble.copy_plugins_and_themes()
     copy_source()
     install_node_modules()
+    end_time = time.time()
+    print(f"{style.SUCCESS} {style.BOLD}Finished in {end_time - start_time:.1f} s.{style.RESET}")
 
 
 if __name__ == "__main__":
